1f885f8cb96a08eba0b653ccdda1edcad68364e2
rmashrur749 <rmashrur749@gmail.com>
2021-04-16 23:56:18 -0500

    added answers to the questions

--- labs/lab9/server.cpp
+++ labs/lab9/server.cpp
@@ -1,157 +1,172 @@
+//Musaddiqur Rahman
+//106932189
 
+
+
+/* 
+
+What is the difference between synchronous and asynchronous communication?
+
+There are a few differences between synchronous and asynchronous communication. 
+The main difference is that synchronous communications happen in the moment, meaning that it’s faster and more dynamic. 
+This means that it’s also good for participation and active discussions with smaller groups.
+
+Conversely, asynchronous communication happens over a period of time, so there’s no need to schedule anything. 
+You can go at your own pace, without as many disruptions. 
+
+Do you prefer socket reads in a receive thread or do you prefer both socket read and write to be in the main() function? Why?
+
+Sockets are not thread safe, because operations included in sockets are not atomic, which means socket read and write operations are independant. And if socket reads
+are done in threads, there will be close() functions as well in those threads which creates the possibility of attempting to run close() operation multiple times, 
+which may cause complications related to file descriptor errors. 
+
+
+*/
 
 #include <iostream>
-#include <netinet/in.h>
-#include <queue>
 #include <signal.h>
 #include <string.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 bool is_running;
-const int BUF_LEN=1024;
-const int MAX_CLIENTS=3;
-void *recv_func(void *arg);
-queue<string> message;
-int  numClients;
-int  master_fd, cl[MAX_CLIENTS];
+const int BUF_LEN = 1024;
+const int MAX_CLIENTS = 3;
+queue < string > message;
+int numClients;
+int fd, cl[MAX_CLIENTS];
 
-/* shared mutex between receive thread and send */
 pthread_mutex_t lock_x;
 
-int main(int argc, char *argv[])
-{
-    //Create the socket for inter-process communications
-    struct sockaddr_in addr;
-    char buf[BUF_LEN];
-    int len;
-    int master_fd,rc;
-    int ret;
+void * recv_func(void * fdArg) {
+        int fd = * (int * ) fdArg;
+        char buffer[BUF_LEN];
+        int max_fd = fd;
+        struct sockaddr_in cli_addr[MAX_CLIENTS];
+        socklen_t clientSize = sizeof(cli_addr[0]);
+        fd_set activefds;
+        fd_set readfds;
 
-    if(argc<2) {
-        cout<<"usage: server <port number>"<<endl;
-	return -1;
-    }
+        FD_ZERO( & readfds);
+        FD_ZERO( & activefds);
+        FD_SET(fd, & activefds);
 
-    //Create the socket
-    memset(&addr, 0, sizeof(addr));
-    if ( (master_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        cout << "server: " << strerror(errno) << endl;
-        exit(-1);
-    }
+        numClients = 0;
+        while (is_running) {
+                readfds = activefds;
+                struct timeval tv;
 
-    //Set the socket to this port
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(atoi(argv[1]));
+                tv.tv_sec = 5;
+                tv.tv_usec = 0;
+                int ret = select(max_fd + 1, & readfds, NULL, NULL, & tv);
+                if (FD_ISSET(fd, & readfds)) { 
+                        cl[numClients] = accept(fd, (struct sockaddr * ) & cli_addr[numClients], & clientSize);
+                        cout << "server: incoming connection " << cl[numClients] << endl;
+                        FD_SET(cl[numClients], & activefds);
+                        if (max_fd < cl[numClients]) max_fd = cl[numClients];
+                        ++numClients;
+                } else if (FD_ISSET(cl[0], & readfds)) {
+                        memset(buffer, 0, BUF_LEN);
+                        ret = read(cl[0], buffer, BUF_LEN);
+                        pthread_mutex_lock( & lock_x);
+                        message.push(buffer);
+                        pthread_mutex_unlock( & lock_x);
+                } else if (FD_ISSET(cl[1], & readfds)) {
+                        memset(buffer, 0, BUF_LEN);
+                        ret = read(cl[1], buffer, BUF_LEN);
+                        pthread_mutex_lock( & lock_x);
 
-    //Bind the socket to this port
-    if (bind(master_fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-        cout << "server: " << strerror(errno) << endl;
-        close(master_fd);
-        exit(-1);
-    }
+                        message.push(buffer);
 
-    cout<<"Waiting for the client..."<<endl;
-    //Listen for a client to connect to this port
-    if (listen(master_fd, 5) == -1) {
-        cout << "server: " << strerror(errno) << endl;
-        close(master_fd);
-        exit(-1);
-    }
-
-    cout<<"client(s) connected to the server"<<endl;
-
-    /* initialize pthread mutex */
-    pthread_mutex_init(&lock_x, NULL);
-
-    /* start the receive thread */
-    pthread_t tid;
-    ret = pthread_create(&tid, NULL, recv_func, &master_fd);
-    if(ret!=0) {
-        cout<<"Cannot create receive thread"<<endl;
-        cout<<strerror(errno)<<endl;
-        close(master_fd);
-        return -1;
-    }
-    is_running = true;
-    while(is_running){
-        while(message.size()>0) {
-	    pthread_mutex_lock(&lock_x);
-            cout<<message.front()<<endl;
-	    message.pop();
-	    pthread_mutex_unlock(&lock_x);
-	}
-        sleep(1);
-    }
-    memset(buf, 0, BUF_LEN);
-    len = sprintf(buf, "Quit")+1;
-    for(int i=0; i<numClients; ++i) {
-        ret = write(cl[i], buf, len);
-        if(ret==-1) {
-            cout<<"server: Write Error"<<endl;
-            cout<<strerror(errno)<<endl;
+                        pthread_mutex_unlock( & lock_x);
+                } else if (FD_ISSET(cl[2], & readfds)) {
+                        memset(buffer, 0, BUF_LEN);
+                        ret = read(cl[2], buffer, BUF_LEN);
+                        pthread_mutex_lock( & lock_x);
+                        message.push(buffer);
+                        pthread_mutex_unlock( & lock_x);
+                } else {
+                        is_running = false;
+                }
         }
-    }
-    pthread_join(tid, NULL);
-    close(master_fd);
-    return 0;
+        pthread_exit(NULL);
 }
 
-void *recv_func(void *arg)
-{
-    int master_fd = *(int *)arg;
-    char buf[BUF_LEN];
-    int  max_fd=master_fd;
-    struct sockaddr_in cli_addr[MAX_CLIENTS];
-    socklen_t clilen = sizeof(cli_addr[0]);
-    fd_set activefds;
-    fd_set readfds;
+int main(int argc, char * argv[]) {
+        struct sockaddr_in addr;
+        char buffer[BUF_LEN];
+        int len;
+        int fd, rc;
+        int ret;
 
-    FD_ZERO(&readfds);
-    FD_ZERO(&activefds);
-    //Add the master_fd to the socket set
-    FD_SET(master_fd, &activefds);
-
-    numClients = 0;
-    while(is_running) {
-        readfds = activefds;
-        struct timeval tv;
-        /* Wait up to five seconds. */
-        tv.tv_sec = 5;
-        tv.tv_usec = 0;
-        int ret = select(max_fd+1, &readfds, NULL, NULL, &tv);
-        if(FD_ISSET(master_fd, &readfds)){//incoming connection
-            cl[numClients] = accept(master_fd, (struct sockaddr *) &cli_addr[numClients], &clilen);
-            cout<<"server: incoming connection "<<cl[numClients]<<endl;
-            FD_SET(cl[numClients], &activefds);
-            if(max_fd<cl[numClients]) max_fd=cl[numClients];
-            ++numClients;
-        } else if(FD_ISSET(cl[0], &readfds)) {
-            memset(buf, 0, BUF_LEN);
-            ret=read(cl[0], buf, BUF_LEN);
-	    pthread_mutex_lock(&lock_x);
-	    message.push(buf);
-	    pthread_mutex_unlock(&lock_x);
-        } else if(FD_ISSET(cl[1], &readfds)) {
-            memset(buf, 0, BUF_LEN);
-            ret=read(cl[1], buf, BUF_LEN);
-	    pthread_mutex_lock(&lock_x);
-	    message.push(buf);
-	    pthread_mutex_unlock(&lock_x);
-        } else if(FD_ISSET(cl[2], &readfds)) {
-            memset(buf, 0, BUF_LEN);
-            ret=read(cl[2], buf, BUF_LEN);
-	    pthread_mutex_lock(&lock_x);
-	    message.push(buf);
-	    pthread_mutex_unlock(&lock_x);
-        } else {
-            is_running=false;
+        if (argc < 2) {
+                cout << "usage: server <port number>" << endl;
+                return -1;
         }
-    }
-    pthread_exit(NULL);
+
+        //Create the socket
+        memset( & addr, 0, sizeof(addr));
+        if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+                cout << "server: " << strerror(errno) << endl;
+                exit(-1);
+        }
+
+        //Set the socket to this port
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = INADDR_ANY;
+        addr.sin_port = htons(atoi(argv[1]));
+        if (bind(fd, (struct sockaddr * ) & addr, sizeof(addr)) == -1) {
+                cout << "server: " << strerror(errno) << endl;
+                close(fd);
+                exit(-1);
+        }
+
+        cout << "Waiting for the client..." << endl;
+        
+        if (listen(fd, 5) == -1) {
+                cout << "server: " << strerror(errno) << endl;
+                close(fd);
+                exit(-1);
+        }
+
+        cout << "client(s) connected to the server" << endl;
+
+        pthread_mutex_init( & lock_x, NULL);
+
+        pthread_t tid;
+        ret = pthread_create( & tid, NULL, recv_func, & fd);
+        if (ret != 0) {
+                cout << "Cannot create receive thread" << endl;
+                cout << strerror(errno) << endl;
+                close(fd);
+                return -1;
+        }
+        is_running = true;
+        while (is_running) {
+                while (message.size() > 0) {
+                        pthread_mutex_lock( & lock_x);
+                        cout << message.front() << endl;
+                        message.pop();
+                        pthread_mutex_unlock( & lock_x);
+                }
+                sleep(1);
+        }
+        memset(buffer, 0, BUF_LEN);
+        len = sprintf(buffer, "Quit") + 1;
+        for (int i = 0; i < numClients; ++i) {
+                ret = write(cl[i], buffer, len);
+                if (ret == -1) {
+                        cout << "server: Write Error" << endl;
+                        cout << strerror(errno) << endl;
+                }
+        }
+        pthread_join(tid, NULL);
+        close(fd);
+        return 0;
 }
